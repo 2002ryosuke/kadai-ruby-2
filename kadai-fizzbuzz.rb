7ec6c323ea164b4fb2b8cddf6e124553ce9b0649
ryosuke <123ryo123456@gmail.com>
2023-02-19 04:19:34 +0000

    make fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,18 @@
+num_max=100
+
+def fizzbuzz(num)
+  if num%3 == 0 && num%5 == 0
+    puts "FizzBuzz"
+  elsif num%3 == 0
+    puts "FIzz"
+  elsif num%5 == 0
+    puts "Buzz"
+  else
+    puts num.to_s
+  end
+end
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
+
